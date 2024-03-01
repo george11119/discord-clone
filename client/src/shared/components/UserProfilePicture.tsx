63832404832cb9548ccea68dad4825c7fd540f46
dotaplayer1 <georgemao72@gmail.com>
2024-03-01 04:25:04 -0800

    Corner status icon

--- client/src/shared/components/UserProfilePicture.tsx
+++ client/src/shared/components/UserProfilePicture.tsx
@@ -1,9 +1,18 @@
 import styled from "styled-components"
 import { useId } from "react"
+import Tooltip from "./Tooltip.tsx"
 
 const Wrapper = styled.div<{ $profileDiameter: number }>`
   position: relative;
   border-radius: 50%;
+`
+
+const StatusIcon = styled.svg<{ $translateDistance: number }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translateX(${(props) => props.$translateDistance}px)
+    translateY(${(props) => props.$translateDistance}px);
 `
 
 const UserProfilePicture = ({
@@ -13,19 +22,53 @@ const UserProfilePicture = ({
 }) => {
   const id = useId()
   const idString = `url(#${id})`
+
+  // css positioning is hard :(
+  // this is disgusting looking but i probably will also never refactor
+
+  // dimensions of the status icon
+  const statusSize =
+    profileDiameter > 60 ? profileDiameter * 0.2 : profileDiameter * 0.3125
+
+  // dimensions of the status icon border
+  const maskSize =
+    profileDiameter > 60
+      ? profileDiameter * 0.3125
+      : profileDiameter * 0.3125 + 4
+
+  // middle of the status icon border
+  const maskTranslateDistance =
+    profileDiameter > 60
+      ? profileDiameter * 0.65 + 3
+      : profileDiameter * 0.65 - 1
+
+  // middle of the status icon
+  const statusIconTranslateDistance =
+    profileDiameter > 60 ? profileDiameter * 0.75 : profileDiameter * 0.6875
+
   return (
     <Wrapper $profileDiameter={profileDiameter}>
-      <svg style={{ height: profileDiameter, width: profileDiameter }}>
-        <mask id={id} width="32" height="32">
-          <circle cx="16" cy="16" r="16" fill="white"></circle>
+      <svg
+        style={{
+          height: profileDiameter,
+          width: profileDiameter,
+        }}
+      >
+        <mask id={id} width="16" height="16">
+          <circle
+            cx={profileDiameter / 2}
+            cy={profileDiameter / 2}
+            r={profileDiameter / 2}
+            fill="white"
+          ></circle>
           <rect
             color="black"
-            x="19"
-            y="19"
-            width="16"
-            height="16"
-            rx="8"
-            ry="8"
+            x={maskTranslateDistance}
+            y={maskTranslateDistance}
+            width={maskSize}
+            height={maskSize}
+            rx={profileDiameter / 4}
+            ry={profileDiameter / 4}
           ></rect>
         </mask>
         {/*TODO replace with foreign object profile picture*/}
@@ -37,6 +80,19 @@ const UserProfilePicture = ({
           mask={idString}
         />
       </svg>
+      <Tooltip tooltip="Do not disturb" placement="top" fontSize={14}>
+        <StatusIcon
+          style={{ height: statusSize, width: statusSize }}
+          $translateDistance={statusIconTranslateDistance}
+        >
+          <circle
+            cx={statusSize / 2}
+            cy={statusSize / 2}
+            r={statusSize / 2}
+            fill="rgb(242, 63, 67)"
+          ></circle>
+        </StatusIcon>
+      </Tooltip>
     </Wrapper>
   )
 }
