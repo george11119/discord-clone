02f38cbf958ce18001094103bf8ff24d69b18f81
dotaplayer1 <georgemao72@gmail.com>
2024-03-05 19:18:25 -0800

    Write tests for /api/auth routes

--- server/test/controllers/users/users.routes.test.ts
+++ server/test/controllers/users/users.routes.test.ts
@@ -1,28 +1,29 @@
 import {
   describe,
   it,
-  beforeEach,
   expect,
   beforeAll,
   afterAll,
+  beforeEach,
 } from "@jest/globals"
 import supertest from "supertest"
-import { server } from "../src/app"
-import { User } from "../src/models/user"
+import { server } from "../../../src/app"
+import { User } from "../../../src/models/user"
 import bcrypt from "bcrypt"
-import { db } from "../src/config/db"
+import { db } from "../../../src/config/db"
 
 const api = supertest(server)
+const url = "/api/users"
 
-beforeAll(async () => {
-  await db.initialize()
-})
+describe(`POST ${url}`, () => {
+  beforeAll(async () => {
+    await db.initialize()
+  })
 
-afterAll(async () => {
-  if (db.isInitialized) await db.destroy()
-})
+  afterAll(async () => {
+    if (db.isInitialized) await db.destroy()
+  })
 
-describe("POST /api/users", () => {
   beforeEach(async () => {
     await User.delete({})
 
@@ -33,7 +34,7 @@ describe("POST /api/users", () => {
     })
   }, 10000)
 
-  it("Successfully creates a new account when email and username are unique and password is valid", async () => {
+  it("Successfully creates new account when email and username are unique and password is valid", async () => {
     const newUser = {
       email: "newuser@test.com",
       username: "newuser",
@@ -41,7 +42,7 @@ describe("POST /api/users", () => {
     }
 
     const res = await api
-      .post("/api/users")
+      .post(`${url}`)
       .send(newUser)
       .expect(201)
       .expect("Content-Type", /application\/json/)
@@ -58,14 +59,27 @@ describe("POST /api/users", () => {
     expect(usersCount).toBe(2)
   })
 
-  it("Doesnt create new account with invalid email", async () => {
+  it("Doesnt create new account with duplicate email", async () => {
     const newUser = {
       email: "existinguser@test.com", // a user with this email already exists
       username: "newuser",
       password: "password",
     }
 
-    await api.post("/api/users").send(newUser).expect(400)
+    await api.post(`${url}`).send(newUser).expect(400)
+
+    const usersCount = await User.count()
+    expect(usersCount).toBe(1)
+  })
+
+  it("Doesnt create new account with duplicate username", async () => {
+    const newUser = {
+      email: "existinguser@test.com",
+      username: "existinguser", // a user with this username already exists
+      password: "password",
+    }
+
+    await api.post(`${url}`).send(newUser).expect(400)
 
     const usersCount = await User.count()
     expect(usersCount).toBe(1)
@@ -78,7 +92,7 @@ describe("POST /api/users", () => {
       password: "a", // password is too short
     }
 
-    await api.post("/api/users").send(newUser).expect(400)
+    await api.post(`${url}`).send(newUser).expect(400)
 
     const usersCount = await User.count()
     expect(usersCount).toBe(1)
